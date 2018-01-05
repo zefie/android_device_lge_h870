047fec8e2192b0f71845b10737eee401af6bab49
zefie <zefie@zefie.net>
2018-01-05 09:54:20 -0500

    initial commit

---
+++ omni.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := h870
+
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/h870/omni_h870.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := h870
+PRODUCT_NAME := h870
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := h870
+PRODUCT_MANUFACTURER := lge
---
+++ omni_h870.mk
@@ -0,0 +1,34 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product-if-exists, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/lge/h870/h870-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/lge/h870/overlay
+
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/lge/h870/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+# Time Zone data for Recovery
+PRODUCT_COPY_FILES += \
+bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+# Release name
+PRODUCT_RELEASE_NAME := h870
+
+PRODUCT_DEVICE := h870
+PRODUCT_NAME := omni_h870
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-H870
+PRODUCT_MANUFACTURER := LGE
+
